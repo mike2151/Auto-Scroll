64e4906c4d90a4196168243d6ad43d0777c579d6
mike2151 <redefiningproductions@gmail.com>
2018-02-17 11:45:56 -0500

    stop animation supported

--- editor.js
+++ editor.js
@@ -1,9 +1,16 @@
-function clickaction() {
-  document.getElementById('instructions').innerHTML = "Instructions Change";
+function scrollaction() {
+  document.getElementById('instructions').innerHTML = "Scroll to stop the automatic scrolling";
+    
+    chrome.tabs.executeScript({
+        code: 'var speed = `'  + String(document.getElementById("speed").value) + '`; var stop=false'
+    }, function(result) {
         chrome.tabs.executeScript({file: "jquery.min.js"}, function(result) {
             chrome.tabs.executeScript({file: "insert.js"}, function(result) {
-            })
+            });
         });
+    }); 
 }
 
-document.getElementById('scroll_button').addEventListener('click', clickaction);
+
+
+document.getElementById('scroll_button').addEventListener('click', scrollaction);
--- insert.js
+++ insert.js
@@ -1,1 +1,10 @@
-$('body,html').animate({ scrollTop: $(document).height() }, 2000);
+var page = $('html, body');
+
+
+page.on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
+       page.stop();
+   });
+
+page.animate({ scrollTop: $(document).height() }, 1000000/(speed + 1), function(){
+       page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+   });
